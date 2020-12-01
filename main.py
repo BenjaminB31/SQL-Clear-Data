37542f1e1c00f0a88adb61d48162307a0dadb225
Benjamin Blanchard <56205597+BenjaminB31@users.noreply.github.com>
2020-12-01 22:01:57 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+tps1 = time.time()
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="HOST",
+  user="USER",
+  password="PASSWORD",
+  database="DATABASE"
+)
+
+table = "TABLE"
+
+request = []
+
+# EXEMPLE
+request.append("Message = 'Donnee a supprimer'")
+request.append("Message like 'Donnee a supp%'")
+
+
+
+for i in request:
+        if request.index(i) < 1 :
+                value = request[0]
+        else:
+                value = str(value) + " OR " + str(i)
+
+
+mycursor = mydb.cursor()
+
+sql = "Delete from " + str(table) + " where " + str(value) + ";"
+mycursor.execute(sql)
+mydb.commit()
+print(mycursor.rowcount, "rows in set.")
+
+mydb.close()
+
+tps2 = time.time()
+print("Execute time :", tps2 - tps1)
